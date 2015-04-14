d0e3d6dade13d7601b021d2cc14da6444da6b903
toxa_black <toxablack@gmail.com>
2015-04-15 00:11:49 +0300

    ver1

---
+++ src/com/toxablack/messenger/server/Constants.java
@@ -0,0 +1,16 @@
+package com.toxablack.messenger.server;
+
+
+public interface Constants {
+
+    /**
+     * Google API Console Browser key.
+     */
+	String API_KEY = "AIzaSyCDfQIJlEPWhIiNEaYErMK5vb6OPkiEqoE";
+
+	String FROM = "email";
+	String REG_ID = "regId";
+	String MSG = "msg";
+	String TO = "email2";
+	
+}
